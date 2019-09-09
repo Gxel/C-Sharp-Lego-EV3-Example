cbcd661231dbe1e5f4a65f7ebfa04e72fd1f5b99
xelorius <gymail@web.de>
2019-09-09 10:14:32 +0200

    The Begin

---
+++ WpfApplicationLegoEV3Ansteuerung1/WpfApplicationLegoEV3Ansteuerung1/FailDialog.xaml.cs
@@ -0,0 +1,191 @@
+﻿/*
+ * BEISPIEL FÜR DIE EINBINDUNG
+ * 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dispatcher.UnhandledException += InitFehler.ExceptionCatcher;
+        }
+    }
+ */
+
+
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Drawing;
+using System.Windows.Media;
+using System.Globalization;
+using System.Net.Mail;
+
+namespace WpfApplicationLegoEV3Ansteuerung1
+{
+    static class InitFehler
+    {
+        public static void ExceptionCatcher(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            FailDialog aFehlerDialog = new FailDialog(e.Exception);
+            aFehlerDialog.ShowDialog();
+        }
+    }
+
+
+
+
+    /// <summary>
+    /// Interaktionslogik für FehlerDialog.xaml
+    /// </summary>
+    public partial class FailDialog : Window
+    {
+        private Exception FException;
+        private string FErrorFilename;
+
+
+        public FailDialog()
+        {
+            InitializeComponent();
+        }
+
+
+        public FailDialog( Exception paramException)
+        {
+            InitializeComponent();
+            this.FException = paramException;
+            FErrorFilename = this.MakeScreenShot();
+            BitmapImage aBitmapImage = new BitmapImage();
+            aBitmapImage.BeginInit();
+            aBitmapImage.UriSource = new Uri(System.IO.Path.Combine(Environment.CurrentDirectory, FErrorFilename));
+            aBitmapImage.EndInit();
+            imageScreenShot1.Source = aBitmapImage;
+            txtboxErrorMsg.Text = paramException.ToString();
+            File.AppendAllText("error.log", DateTime.Now + Environment.NewLine + paramException + Environment.NewLine + Environment.NewLine + Environment.NewLine);
+        }
+
+
+
+        public string MakeScreenShot()
+        {
+            string filename = String.Empty;
+            double screenLeft = SystemParameters.VirtualScreenLeft;
+            double screenTop = SystemParameters.VirtualScreenTop;
+            double screenWidth = SystemParameters.VirtualScreenWidth;
+            double screenHeight = SystemParameters.VirtualScreenHeight;
+            using (Bitmap aBitmap1 = new Bitmap( (int)screenWidth, ((int)screenHeight)*2 ) )
+            {
+                using (Graphics aSingleGraphics1 = Graphics.FromImage(aBitmap1))
+                {
+                    filename = "error_" + DateTime.Now.ToString("yyMMdd_Hmmss") + ".png";
+                    aSingleGraphics1.CopyFromScreen( ((int)screenLeft), ((int)screenTop) * 2, 0, 0, aBitmap1.Size);
+                    System.Drawing.Point aPoint1 = new System.Drawing.Point(0, ((int)screenHeight)+30);
+                    aSingleGraphics1.DrawString("Fehlermeldung : " + FException.Message, new Font("Arial", 24), System.Drawing.Brushes.Red, aPoint1);
+                    aPoint1.Y += 100;
+                    aSingleGraphics1.DrawString("Benutzername : " + Environment.UserName, new Font("Arial", 24), System.Drawing.Brushes.Red, aPoint1);
+                    aPoint1.Y += 40;
+                    aSingleGraphics1.DrawString("Betriebssystem : " + Environment.OSVersion, new Font("Arial", 24), System.Drawing.Brushes.Red, aPoint1);
+                    aPoint1.Y += 40;
+                    aSingleGraphics1.DrawString("Computer : " + Environment.MachineName, new Font("Arial", 24), System.Drawing.Brushes.Red, aPoint1);
+                    aPoint1.Y += 40;
+                    aSingleGraphics1.DrawString("Version der Applikation : " + Environment.Version, new Font("Arial", 24), System.Drawing.Brushes.Red, aPoint1);
+                    aPoint1.Y += 40;
+                    aSingleGraphics1.DrawString("StackTrace : " + Environment.NewLine + FException.ToString(), new Font("Arial", 14), System.Drawing.Brushes.Red, aPoint1);
+                    aBitmap1.Save(filename);
+                }
+            }
+            return filename;
+        }
+
+
+
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = "png";
+            saveFileDialog.Filter = "Bild (*.png)| *.png";
+            saveFileDialog.FileName = FErrorFilename;
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.Copy(System.IO.Path.Combine(Environment.CurrentDirectory, FErrorFilename), saveFileDialog.FileName, true);
+            }            
+            buttonSave1.IsEnabled = false;
+        }
+
+
+
+
+
+        private void buttonSend1_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MailMessage aMailMessage = new MailMessage();
+                aMailMessage.From = new MailAddress("absender@domain.de"); //Absender 
+                aMailMessage.To.Add("emfang@domain.de"); //Empfänger 
+                aMailMessage.Subject = "Das ist der Betreff";
+                aMailMessage.Body = "Der Inhalt";
+                if (chkBoxSendScreenShot.IsChecked == true)
+                {
+                    aMailMessage.Attachments.Add(new System.Net.Mail.Attachment(System.IO.Path.Combine(Environment.CurrentDirectory, FErrorFilename)));
+                }
+                //mail.IsBodyHtml = true; //Nur wenn Body HTML Quellcode ist  
+                SmtpClient client = new SmtpClient("smtp.live.com", 25); //SMTP Server von Hotmail und Outlook. 
+                try
+                {
+                    client.Credentials = new System.Net.NetworkCredential("absender@domain.de", "Passwort");//Anmeldedaten für den SMTP Server 
+                    client.EnableSsl = true; //Die meisten Anbieter verlangen eine SSL-Verschlüsselung 
+                    client.Send(aMailMessage); //Senden 
+                    buttonSend1.IsEnabled = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "E-Mail senden fehlgeschlagen.");
+                }
+
+            }
+            catch(Exception ex)
+            {
+                string s = string.Empty;
+                s += "Fehler E-Mail kann nicht gesendet werden." + Environment.NewLine;
+                s += ex.Message;
+                MessageBox.Show(s);
+            }
+        }
+
+
+
+
+
+        private void buttonProgrammAbbruch1_Click(object sender, RoutedEventArgs e)
+        {
+            string str = Process.GetCurrentProcess().ProcessName;
+            Process[] pp = Process.GetProcessesByName(str);
+            foreach (Process p in pp)
+            {
+                //p.CloseMainWindow();
+                p.Kill();
+            }
+        }
+
+
+
+
+        private void buttonTrotzdemWeiter1_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ WpfApplicationLegoEV3Ansteuerung1/WpfApplicationLegoEV3Ansteuerung1/MainWindow.xaml.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+namespace WpfApplicationLegoEV3Ansteuerung1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Brick brick;
+        Action aAction;
+        Task aTask;
+
+
+        public void Hallo()
+        {
+
+        }
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Dispatcher.UnhandledException += InitFehler.ExceptionCatcher;
+
+            aAction = new Action(Hallo);
+            aTask = new Task(aAction);
+
+            ComboBoxItem aListItem = new ComboBoxItem();
+            comboBoxComPort1.Items.Insert(0, "Com1");
+            comboBoxComPort1.Items.Insert(1, "Com2");
+            comboBoxComPort1.Items.Insert(2, "Com3");
+            comboBoxComPort1.Items.Insert(3, "Com4");
+            comboBoxComPort1.Items.Insert(4, "Com5");
+            comboBoxComPort1.Items.Insert(5, "Com6");
+            comboBoxComPort1.Items.Insert(6, "Com7");
+            comboBoxComPort1.Items.Insert(7, "Com8");
+            comboBoxComPort1.Items.Insert(8, "Com9");
+
+            comboBoxComPort1.SelectedIndex = 0;
+
+            gridBottom1.Visibility = Visibility.Hidden;
+        }
+
+
+
+        private void buttonVerbinden_Click(object sender, RoutedEventArgs e)
+        {
+            ComboBoxItem aComboBoxItem = comboBoxCommunication1.SelectedItem as ComboBoxItem;
+            if (aComboBoxItem.Content.ToString() == "USB")
+            {
+                UsbCommunication aUsbCommunication = new UsbCommunication();
+                brick = new Brick(aUsbCommunication, true);
+            }
+            else
+            {
+                BluetoothCommunication aBluetoothCommunication1 = new BluetoothCommunication(comboBoxComPort1.SelectedValue.ToString());
+                brick = new Brick(aBluetoothCommunication1, true);
+            }
+            // ---
+            try
+            {
+                aTask = brick.ConnectAsync();
+                lblStatusBarMessage1.Text = aTask.Status.ToString();        
+                
+                if (aTask.Status != TaskStatus.Faulted )
+                {
+                    gridBottom1.Visibility = Visibility.Visible;
+                }                
+            }
+            catch ( Exception ex)
+            {
+                lblStatusBarMessage1.Text = "Exception: " + ex.Message ;
+            }
+        }
+
+
+        private void brick_BrickChanged(object sender, BrickChangedEventArgs e)
+        {
+            textBoxReturns.Text = "-----------------------------------------------------------------------" + Environment.NewLine + textBoxReturns.Text;
+            textBoxReturns.Text = "Sensorwert 1 (One)  :  " + brick.Ports[InputPort.One].SIValue.ToString() + Environment.NewLine + textBoxReturns.Text;
+            textBoxReturns.Text = "Sensorwert 2 (Two)  :  " + brick.Ports[InputPort.Two].SIValue.ToString() + Environment.NewLine + textBoxReturns.Text;
+            textBoxReturns.Text = "Sensorwert 3 (Three):  " + brick.Ports[InputPort.Three].SIValue.ToString() + Environment.NewLine + textBoxReturns.Text;
+            textBoxReturns.Text = "Sensorwert 4 (Four) :  " + brick.Ports[InputPort.Four].SIValue.ToString() + Environment.NewLine + textBoxReturns.Text;
+        }
+
+
+
+
+        private void buttonGo_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+
+        private void comboBoxCommunication1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBoxComPort1 != null)
+            {
+                ComboBoxItem aComboBoxItem = comboBoxCommunication1.SelectedItem as ComboBoxItem;
+                if (aComboBoxItem.Content.ToString() == "USB")
+                {
+                    comboBoxComPort1.IsEnabled = false;
+                }
+                else
+                {
+                    comboBoxComPort1.IsEnabled = true;
+                }
+            }
+        }
+
+
+
+        private void buttonTrennen_Click(object sender, RoutedEventArgs e)
+        {
+            brick.Disconnect();
+        }
+
+
+
+        private void buttonStatus1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(aTask.Status.ToString());
+        }
+
+        private void buttonGoA_Click(object sender, RoutedEventArgs e)
+        {
+            brick.DirectCommand.StepMotorAtSpeedAsync(OutputPort.A, Convert.ToInt32(sliderSpeedA.Value), Convert.ToUInt32(sliderSteppsA.Value), true);
+        }
+
+        private void buttonGoB_Click(object sender, RoutedEventArgs e)
+        {
+            brick.DirectCommand.StepMotorAtSpeedAsync(OutputPort.B, Convert.ToInt32(sliderSpeedB.Value), Convert.ToUInt32(sliderSteppsB.Value), true);
+        }
+
+        private void buttonGoC_Click(object sender, RoutedEventArgs e)
+        {
+            brick.DirectCommand.StepMotorAtSpeedAsync(OutputPort.C, Convert.ToInt32(sliderSpeedC.Value), Convert.ToUInt32(sliderSteppsC.Value), true);
+        }
+
+        private void buttonGoD_Click(object sender, RoutedEventArgs e)
+        {
+            brick.DirectCommand.StepMotorAtSpeedAsync(OutputPort.D, Convert.ToInt32(sliderSpeedD.Value), Convert.ToUInt32(sliderSteppsD.Value), true);
+        }
+
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            brick.DirectCommand.StopMotorAsync(OutputPort.A, true);
+            brick.DirectCommand.StopMotorAsync(OutputPort.B, true);
+            brick.DirectCommand.StopMotorAsync(OutputPort.C, true);
+            brick.DirectCommand.StopMotorAsync(OutputPort.D, true);
+        }
+
+        private void handleSlider(Label aLabel, Slider aSlider)
+        {
+            if (aSlider != null & aLabel != null)
+            {
+                int i = Convert.ToInt32(aSlider.Value);
+                aLabel.Content = i.ToString();
+            }
+        }
+
+
+
+
+        private void sliderSpeedA_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            handleSlider(labelSpeedA, sliderSpeedA);
+        }
+
+        private void sliderSteppsA_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            handleSlider(labelSteppsA, sliderSteppsA);
+        }
+
+        private void sliderSpeedBy_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            handleSlider(labelSpeedB, sliderSpeedB);
+        }
+
+        private void sliderSteppsB_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            handleSlider(labelSteppsB, sliderSteppsB);
+        }
+
+        private void sliderSpeedC_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            handleSlider(labelSpeedC, sliderSpeedC);
+        }
+
+        private void sliderSteppsC_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            handleSlider(labelSteppsC, sliderSteppsC);
+        }
+
+        private void sliderSpeedD_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            handleSlider(labelSpeedD, sliderSpeedD);
+        }
+
+        private void sliderSteppsD_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            handleSlider(labelSteppsD, sliderSteppsD);
+        }
+
+        private void buttonSensor1_Click(object sender, RoutedEventArgs e)
+        {
+            BrickChangedEventArgs aBrickChangedEventArgs = new BrickChangedEventArgs();
+            textBoxReturns.Text = "Sensorwert 1 (One):  " +brick.Ports[InputPort.One].SIValue.ToString() + Environment.NewLine + textBoxReturns.Text;
+        }
+
+        private void buttonSensor2_Click(object sender, RoutedEventArgs e)
+        {
+            BrickChangedEventArgs aBrickChangedEventArgs = new BrickChangedEventArgs();
+            textBoxReturns.Text = "Sensorwert 2 (Two):  " + brick.Ports[InputPort.Two].SIValue.ToString() + Environment.NewLine + textBoxReturns.Text;
+        }
+
+        private void buttonSensor3_Click(object sender, RoutedEventArgs e)
+        {
+            BrickChangedEventArgs aBrickChangedEventArgs = new BrickChangedEventArgs();
+            textBoxReturns.Text = "Sensorwert 3 (Three):  " + brick.Ports[InputPort.Three].SIValue.ToString() + Environment.NewLine + textBoxReturns.Text;
+        }
+
+        private void buttonSensor4_Click(object sender, RoutedEventArgs e)
+        {
+            BrickChangedEventArgs aBrickChangedEventArgs = new BrickChangedEventArgs();
+            textBoxReturns.Text = "Sensorwert 4 (Four):  " + brick.Ports[InputPort.Four].SIValue.ToString() + Environment.NewLine + textBoxReturns.Text;
+        }
+
+        private void buttonSensorsAll_Click(object sender, RoutedEventArgs e)
+        {
+            brick.BrickChanged += brick_BrickChanged;
+        }
+
+        private void buttonIsCheckedGo_Click(object sender, RoutedEventArgs e)
+        {
+            if (checkBoxMotorA.IsChecked.Value)
+            {
+                buttonGoA_Click(sender,e);
+            }
+
+
+            if (checkBoxMotorB.IsChecked.Value)
+            {
+                buttonGoB_Click(sender, e);
+            }
+
+
+            if (checkBoxMotorC.IsChecked.Value)
+            {
+                buttonGoC_Click(sender, e);
+            }
+
+
+            if (checkBoxMotorD.IsChecked.Value)
+            {
+                buttonGoD_Click(sender, e);
+            }
+        }
+    }
+}
